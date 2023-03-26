652e1ccf40a10c935e071409152e3448dd152dc1
JonathanMarquezIbarra <jonathan.marquib@gmail.com>
2023-03-25 20:35:34 -0600

    basic recyclerview

---
+++ app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.recyclerview.adapter
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.R
+import com.example.recyclerview.SuperHero
+
+class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName)
+    val realName = view.findViewById<TextView>(R.id.tvRealName)
+    val publisher = view.findViewById<TextView>(R.id.tvPublisher)
+    val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
+
+
+    fun render(superHeroModel: SuperHero) {
+        superHero.text = superHeroModel.superHero
+        realName.text = superHeroModel.realName
+        publisher.text = superHeroModel.publisher
+    }
+}
