65e1f73146de69e825f9f3ace024a6f4003ea327
JonathanMarquezIbarra <jonathan.marquib@gmail.com>
2023-03-25 20:48:33 -0600

    glide implementation for show images

--- app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.recyclerview.R
 import com.example.recyclerview.SuperHero
 
@@ -18,5 +19,6 @@ class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
         superHero.text = superHeroModel.superHero
         realName.text = superHeroModel.realName
         publisher.text = superHeroModel.publisher
+        Glide.with(photo.context).load(superHeroModel.photo).into(photo)
     }
 }
