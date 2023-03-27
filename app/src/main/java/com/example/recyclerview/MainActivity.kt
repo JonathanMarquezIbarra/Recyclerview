c99ffd02080043dea07ac7e573f6042d92df9c4e
JonathanMarquezIbarra <jonathan.marquib@gmail.com>
2023-03-26 19:00:17 -0600

    change linear view for grid view.

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerview.adapter.SuperHeroAdapter
 import com.example.recyclerview.databinding.ActivityMainBinding
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView(){
-        val manager = LinearLayoutManager(this)
+        val manager = GridLayoutManager(this, 2)
         val decoration = DividerItemDecoration(this, manager.orientation)
 
         binding.recyclerSuperHero.layoutManager = manager
